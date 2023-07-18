1c15e757657fb2f9e9c9a158f94cf62cb32d2995
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-07-18 23:46:48 +0530

    feature - create loader component and implement it

---
+++ src/shared/loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loader = () => {
+    return (
+        <>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        </>
+    )
+}
+
+export default Loader;  
